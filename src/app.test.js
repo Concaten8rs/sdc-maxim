bdf8969441216e8a45709516c0aae6ad9e35f8da
dmondo <dannysmondo@gmail.com>
2020-05-01 17:37:09 -0700

    test(app.test): smoke test for enzyme

--- src/app.test.js
+++ src/app.test.js
@@ -2,3 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './app.jsx';
 
+describe('My Test Suite', () => {
+  it('My Test Case', () => {
+    expect(true).toEqual(true);
+  });
+});
+
+describe('First React component test with Enzyme', () => {
+  it('renders without crashing', () => {
+     shallow(<App />);
+   });
+});
