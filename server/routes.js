192838d1886e9108fa3b9bb629fb238cd0b6745c
dmondo <dannysmondo@gmail.com>
2020-05-03 12:25:10 -0700

    refactor(server): static assets served from dist folder

--- server/routes.js
+++ server/routes.js
@@ -1,17 +1,10 @@
 const router = require('express').Router();
+const path = require('path');
 
 const db = require('./db');
 
 router.get('/', (req, res) => {
-  db.findAll((err, data) => (
-    err ? res.sendStatus(500) : res.json(data)
-  ));
-});
-
-router.post('/', (req, res) => {
-  db.save(req.body, (err) => (
-    err ? res.sendStatus(500) : res.sendStatus(200)
-  ));
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 router.get('/reviews/:product', (req, res) => {
