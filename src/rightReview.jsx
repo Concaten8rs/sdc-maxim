8447e8d77e56c0c0b6b8c0447641f28785164f4e
dmondo <dannysmondo@gmail.com>
2020-05-04 13:47:07 -0700

    style(rightWidget): css styling and prop passing for child styling

--- src/rightReview.jsx
+++ src/rightReview.jsx
@@ -4,15 +4,15 @@ import SizeWidget from './sizeWidget.jsx';
 
 const RightReview = ({review}) => (
   <div className='side-wrapper'>
-  <div className='rating-widgets'>
+  <div className='rating-widgets widget-wrapper'>
     <div>
-      <RatingWidget val={review.comfort} />
+      <RatingWidget val={review.comfort} type={'Comfort'} />
     </div>
     <div>
-      <RatingWidget val={review.style} />
+      <RatingWidget val={review.style} type={'Style'} />
     </div>
     <div>
-      <RatingWidget val={review.value} />
+      <RatingWidget val={review.value} type={'Value'} />
     </div>
     <div>
       <SizeWidget val={review.sizing} />
