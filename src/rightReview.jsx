7ecbc0fc4945d3632d9cb067540f78238215f65b
dmondo <dannysmondo@gmail.com>
2020-05-04 15:34:07 -0700

    refactor(rightReview): right side of review card is modularized

--- src/rightReview.jsx
+++ src/rightReview.jsx
@@ -1,36 +1,27 @@
 import React from 'react';
 import RatingWidget from './ratingWidget.jsx';
 import SizeWidget from './sizeWidget.jsx';
+import Helpful from './helpful.jsx';
 
+//TODO: remove extraneous divs
 const RightReview = ({review}) => (
   <div className='side-wrapper'>
-  <div className='rating-widgets'>
-    <div>
-      <RatingWidget val={review.comfort} />
+    <div className='rating-widgets'>
+      <div>
+        <RatingWidget val={review.comfort} />
+      </div>
+      <div>
+        <RatingWidget val={review.style} />
+      </div>
+      <div>
+        <RatingWidget val={review.value} />
+      </div>
+      <div>
+        <SizeWidget val={review.sizing} />
+      </div>
     </div>
-    <div>
-      <RatingWidget val={review.style} />
-    </div>
-    <div>
-      <RatingWidget val={review.value} />
-    </div>
-    <div>
-      <SizeWidget val={review.sizing} />
-    </div>
+    <Helpful />
   </div>
-  <div className='help'>
-    4 guests found this review helpful. Did you?
-    <div>
-      helpful
-    </div>
-    <div>
-      not helpful
-    </div>
-  </div>
-  <div className='report'>
-    Report review
-  </div>
-</div>
 );
 
 export default RightReview;
