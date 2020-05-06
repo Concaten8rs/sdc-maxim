8066ec3a15dd6c80b45de4cbd400cfc7309883c2
dmondo <dannysmondo@gmail.com>
2020-05-05 17:14:02 -0700

    tests: increase test coverage

--- enzyme.config.js
+++ enzyme.config.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+require('jest-fetch-mock').enableMocks();
+
 configure({ adapter: new Adapter() });
--- tests/review.test.js
+++ tests/review.test.js
@@ -4,8 +4,21 @@ import Review from '../src/review.jsx';
 
 describe('Review component', () => {
   it('accepts review prop', () => {
-    const wrapper = mount(<Review review={{product: 'shoe'}} />);
-    expect(wrapper.props().review.product).toEqual('shoe');
+    const wrapper = mount(<Review review={{
+      product: 'pants',
+      title: 'hi',
+      name: 'a',
+      stars: 4,
+      verified: true,
+      date: new Date(),
+      content: 'i dont like',
+      comfort: 1,
+      style: 1.2,
+      value: 5,
+      sizing: 'too small',
+      photo: 'false',
+    }} />);
+    expect(wrapper.props().review.product).toEqual('pants');
     wrapper.unmount();
   });
 });
