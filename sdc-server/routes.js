a2da4acd2c9a68895d0a1be02f60a5b5a0d3f0a4
Maxim Rietveld <maxrietveld@gmail.com>
2020-05-30 11:33:28 -0700

    succesfully connected to cassandraDB

---
+++ sdc-server/routes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const path = require('path');
+
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+router.get('/reviews/all', (req, res) => {
+
+});
+
+router.get('/reviews/:product', (req, res) => {
+  db.find(req.params.product, (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data.reviews);
+    }
+  });
+});
+
+router.get('/reviews/stars/:product', (req, res) => {
+  db.find(req.params.product, (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data.stars);
+    }
+  });
+});
+
+router.get('/rev', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+router.get('/rev/bundle.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/bundle.js'));
+});
+
+module.exports = router;
