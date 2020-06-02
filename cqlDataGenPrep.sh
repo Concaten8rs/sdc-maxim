abe95d6244d461e4bbb95c7dc6ccf17e04f2c1fc
Maxim Rietveld <maxrietveld@gmail.com>
2020-06-01 17:30:13 -0700

    added file structure bash script for data

---
+++ cqlDataGenPrep.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir ./sdc-server/lib/data
+
+touch ./sdc-server/lib/data/cqlProductData.csv
+touch ./sdc-server/lib/data/cqlReviewData1.csv
+touch ./sdc-server/lib/data/cqlReviewData2.csv
